5e4301d8201deab007212171251755f87a287388
addityap <addityap06@gmail.com>
2021-06-07 19:00:40 +0700

    form pertama

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,6 +1,26 @@
 @extends('layouts.index')
 @section('content')
-<h1>
-    Hellos
-</h1>
+<div class="container">
+    <div class="row">
+    <div class="d-flex justify-content-center">
+        <div class="card w-100">
+            <div class="card">
+                <div class="card-title">
+                    Form Permintaan barang    
+                </div>
+                <div class="card-body">
+                <form action="" method="POST">
+                @csrf
+                
+                <div class="form-group">
+                    <label for="nama">Nama</label>
+                    <input type="text" id="nama" name="nama" class="form-control">
+                </div>
+                </form>    
+                </div>    
+            </div>    
+        </div>    
+    </div>    
+    </div>    
+</div>
 @endsection
