5e6c1e3cb373b2f91feb6cced575db142b461200
addityap <addityap06@gmail.com>
2021-06-07 19:50:17 +0700

    Layouting

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -9,14 +9,15 @@
                     Form Permintaan barang    
                 </div>
                 @if (session('success'))    
-                    <div class="alert alert-success" role="alert">
+                <div class="alert alert-success" role="alert">
                         {{session('success')}}
                 @endif
                 </div>
+                   
+                <div class="card-body">
                     <div class="text-end mt-3 mr-3">
                         <a href="{{route('permintaanbrg.index')}}" class="btn btn-success">Show Data</a>
                     </div>
-                <div class="card-body">
                 <form action="{{route('permintaanbrg.store')}}" method="POST">
                 @csrf
                 <div class="form-group">
